b6c45bde68de04d6d223b93cd3bd5fba138f9cbd
Teodor <teodor.orlinov.trendafilov@gmail.com>
2025-06-14 13:59:33 +0300

    Added sample test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+    @Test
+    void sample() {
+        assertEquals(2, 1 + 1);
+    }
+}
